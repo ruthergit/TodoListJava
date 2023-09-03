624600d8e9c552d12e69bb605069a42201658013
Ruther <ruthermrcs@gmail.com>
2023-09-03 13:22:49 +0800

    updated code for todolist

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 public class Main extends JFrame {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
--- src/mainFrame.java
+++ src/mainFrame.java
@@ -16,6 +16,8 @@ public class mainFrame extends JFrame {
     protected JTextField addTaskTextField;
     protected JTextField[] taskFields;
     protected JButton[] doneButtons;
+    private JButton addTaskBtn; 
+
 
     public mainFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,8 +28,67 @@ public class mainFrame extends JFrame {
         setResizable(false);
         setContentPane(contentPane);
         contentPane.setLayout(null);
+        
+        JLabel lblNewLabel = new JLabel("To-Do List");
+        lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 26));
+		lblNewLabel.setForeground(new Color(254, 231, 21));
+		lblNewLabel.setBounds(10, 11, 167, 40);
+		contentPane.add(lblNewLabel);
 
-        // Rest of the code from the second part
-        // ... (addTaskTextField, addTaskBtn, labels, task fields, done buttons)
+        addTaskTextField = new JTextField();
+        addTaskTextField.setForeground(new Color(254, 231, 21));
+		addTaskTextField.setFont(new Font("Verdana", Font.BOLD, 14));
+		addTaskTextField.setBackground(new Color(16, 24, 32));
+		addTaskTextField.setBounds(10, 76, 321, 28);
+		contentPane.add(addTaskTextField);
+		addTaskTextField.setColumns(10);
+		
+		addTaskBtn = new JButton("ADD TASK");
+        addTaskBtn.setBackground(new Color(16, 24, 32));
+		addTaskBtn.setForeground(new Color(254, 231, 21));
+		addTaskBtn.setFocusable(false);
+		addTaskBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				taskFields[0].setText(addTaskTextField.getText());
+			}
+		}); 
+		contentPane.add(addTaskBtn);
+
+
+        taskFields = new JTextField[6];
+        doneButtons = new JButton[6];
+
+        for (int i = 0; i < 6; i++) {
+            taskFields[i] = new JTextField();
+            taskFields[i].setForeground(new Color(254, 231, 21));
+            taskFields[i].setFont(new Font("Verdana", Font.BOLD, 14));
+            taskFields[i].setColumns(10);
+            taskFields[i].setBackground(new Color(16, 24, 32));
+            taskFields[i].setBounds(10, 152, 321, 28);
+            taskFields[i].setEditable(false);
+
+            doneButtons[i] = new JButton("Done");
+            // ... (Set properties and bounds)
+            final int index = i; // Need to use final for ActionListener
+            doneButtons[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    // Handle Done button click
+                }
+            });
+            
+            contentPane.add(taskFields[i]);
+            contentPane.add(doneButtons[i]);
+        }
+
+        // Set layout positions
+        setLayoutPositions();
+    }
+
+    private void setLayoutPositions() {
+        
+    	addTaskBtn.setBounds(341, 78, 105, 23);
+        
+    
+
     }
 }
